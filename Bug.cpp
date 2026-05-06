c39d56eeba30026bc66948d6f0beb304fdfbe436
SpicyChicken75 <D00280023@student.dkit.ie>
2026-05-06 19:41:41 +0100

    Made the rest of files

--- Bug.cpp
+++ Bug.cpp
@@ -1,3 +1,18 @@
 //
 // Created by Elvis on 06/05/2026.
 //
+
+#include "Bug.h"
+
+Bug::Bug(int id, int x, int y, int dir, int health) {
+    this->id = id;
+    position = {x,y};
+    direction = dir;
+    this->health = health;
+    alive = true;
+    path.push_back(position);
+}
+
+int Bug::getId() { return id; }
+pair<int,int> Bug::getPosition() { return position; }
+bool Bug::isAlive() { return alive; }
