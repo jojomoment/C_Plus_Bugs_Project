6015d8fe686f109f78cdfe52e408301f72879dd4
SpicyChicken75 <D00280023@student.dkit.ie>
2026-05-06 19:12:35 +0100

    Bug made

---
+++ Bug.h
@@ -0,0 +1,31 @@
+//
+// Created by Elvis on 06/05/2026.
+//
+
+#pragma once
+#include <utility>
+#include <list>
+using namespace std;
+
+class Bug {
+protected:
+    int id;
+    pair<int,int> position;
+    int direction;
+    int health;
+    bool alive;
+    list<pair<int,int>> path;
+
+public:
+    Bug(int id, int x, int y, int dir, int health);
+
+    virtual void move() = 0; // PURE virtual
+
+    bool isWayBlocked();
+
+    int getId();
+    pair<int,int> getPosition();
+    bool isAlive();
+};
+
+
