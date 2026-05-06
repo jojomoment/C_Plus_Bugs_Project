936347e2c875470d3613e97ede661a15d605be25
jojomoment <kamiljamaljojo@gmail.com>
2026-05-06 19:44:01 +0100

    Crawler.cpp made and populated

---
+++ Crawler.cpp
@@ -0,0 +1,11 @@
+#include "Crawler.h"
+#include <iostream>
+
+using namespace std;
+
+//constructor definition
+Crawler::Crawler(int id,int x,int y,int direction,int health)
+    : Bug(id, x, y, direction, health)
+{
+
+}
