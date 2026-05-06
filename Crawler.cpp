5b8f56a4566f5f439d45bbebc756057b79a11beb
jojomoment <kamiljamaljojo@gmail.com>
2026-05-06 19:53:15 +0100

    implemented move function with boundaries

--- Crawler.cpp
+++ Crawler.cpp
@@ -9,3 +9,34 @@ Crawler::Crawler(int id,int x,int y,int direction,int health)
 {
 
 }
+
+
+void Crawler::move()
+{
+    // North
+    if(direction == 1 && position.second > 0)
+    {
+        position.second--;
+    }
+
+    // East
+    else if(direction == 2  && position.first < 9)
+    {
+        position.first++;
+    }
+
+    // South
+    else if(direction == 3 && position.second < 9)
+    {
+        position.second++;
+    }
+
+    // West
+    else if(direction == 4 && position.first > 0)
+    {
+        position.first--;
+    }
+
+    // Save new position to path
+    path.push_back(position);
+}
